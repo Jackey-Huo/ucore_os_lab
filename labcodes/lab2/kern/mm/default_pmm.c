d23d890a4ea6855855664355381cb2fbd5214944
Jackey-Huo <jackeyhuo15@gmail.com>
2019-04-16 15:22:28 +0800

    fix realloc but in default_alloc_pages

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -138,6 +138,7 @@ default_alloc_pages(size_t n) {
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
+            SetPageProperty(p);
             list_add_after(&(page->page_link), &(p->page_link));
         }
         list_del(&(page->page_link));
